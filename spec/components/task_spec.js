7a37f3cd8da084035e36f3528aa5da4a30000d31
Edward Minnett <ed@methodic.io>
2016-07-07 20:24:37 +0100

    Test: Add Task component spec
    Connects to #34

---
+++ spec/components/task_spec.js
@@ -0,0 +1,23 @@
+import expect from 'expect';
+import expectJSX from 'expect-jsx';
+import React from 'react';
+import TestUtils from 'react-addons-test-utils';
+import Task from '../../components/subtask';
+
+expect.extend(expectJSX);
+
+describe('Task', () => {
+  const renderedTask = function renderedTask() {
+    const renderer = TestUtils.createRenderer();
+    renderer.render(<Task />);
+    return renderer.getRenderOutput();
+  };
+
+  it('includes a checkbox', () => {
+    expect(renderedTask()).toIncludeJSX(<input type="checkbox" />);
+  });
+
+  it('includes a label', () => {
+    expect(renderedTask()).toIncludeJSX(<div className="label">Subtask Label</div>);
+  });
+});
