d04b72c9ef8634201be1d1b4a812fbb8251a28d9
Edward Minnett <ed@methodic.io>
2016-07-07 20:25:25 +0100

    Feat: Add basic Subtask component
    Connects to #33

---
+++ components/subtask.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Subtask = function Subtask() {
+  return (
+    <div className="subtask">
+      <input type="checkbox" />
+      <div className="label">Subtask Label</div>
+    </div>
+  );
+};
+
+export default Subtask;
