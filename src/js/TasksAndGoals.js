aee279eaf1d21f291649faf579b9f0883af90613
Edward Minnett <ed@methodic.io>
2016-07-13 18:59:06 +0100

    Feat: Add application entry point js files
    Connects to #40

---
+++ src/js/TasksAndGoals.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Group from './components/group';
+// import ReactDOM from 'react-dom';
+
+const TasksAndGoals = function TasksAndGoals() {
+  return (
+    <div className="tasks-and-goals">
+      <Group />
+    </div>
+  );
+};
+
+export default TasksAndGoals;
---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TasksAndGoals from './TasksAndGoals';
+
+require('../styles/app.scss');
+
+ReactDOM.render(<TasksAndGoals />, document.getElementById('app'));
