cdd37e05ec9ed5b85990579a1d23956348390acf
Edward Minnett <ed@methodic.io>
2016-07-18 09:40:10 +0100

    Feat: Add the InjectTapEventPlugin initialisation to App.js
    Connects to #52

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import TasksAndGoals from './TasksAndGoals';
+
+// Needed for onTouchTap (this is a pre-requisite for Material UI)
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
 
 require('../styles/app.scss');
 
